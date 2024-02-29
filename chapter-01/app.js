23c911746c5b6e042ad96dabd3b20ff325462e9b
Muhammad Faizan <fiazansuri855@gmail.com>
2024-02-29 23:50:30 +0500

    JS-chapter-01

---
+++ chapter-01/app.js
@@ -0,0 +1,55 @@
+//   chapter-no-1
+
+//    Q.no-01 : Write a script to greet your website visitor using JS alert box ?  
+
+    //   Ans.no-01
+
+    alert ('');
+
+
+    //    Q.no-2 : Write a script to display following message on your web page ?
+    
+    //    ANs.no-2
+       
+    alert('Please Enter a valid password');
+    
+     
+    //    Q.no-03 : Write a script to display following message on your web 
+    //                 page: (Hint : Use line break) ?
+    
+        //  Ans.No-03 
+    
+       alert('Welcome to JS land...\nhappy coding!'); 
+    
+    
+        //   Q.no-04 : Write a script to display following messages in sequence:
+    
+    
+        // Ans.no-04
+    
+    
+        alert('Welcome to JS land');
+        alert('Happy Coding');
+    
+    
+        // Q.no-05 : Generate the following message through browser’s developer console:
+    
+        // Ans.no-05
+    
+        console.log("Hello...I can run JS through my web browser's console");
+    
+    
+        // Q.no-06 : Make use of alerts in your new/existing HTML & CSS project.
+    
+        // Ans.no-06
+    
+        // <script src="app.js"></script>
+    
+    
+        // Q.no-07 : Practice placement of <script></script> element in  
+        // following sections of your project in exercise 6:
+    
+         // Ans.no-07
+     
+         // Body (before your page’s HTML)
+    
