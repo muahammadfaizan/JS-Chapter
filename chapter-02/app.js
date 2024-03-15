cc754abc385094da5ca3c2487f8e49b5f9f82290
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-16 00:22:28 +0500

    JS-chapter-02 assignment

---
+++ chapter-02/app.js
@@ -0,0 +1,102 @@
+  // chapter 02
+
+    // Question & Answer
+
+//  Q.no-01 : Declare a variable called username.
+
+    // Ans.01
+
+    var userName = "Muhammad Faizan";
+
+    //    Q.no-02 : Declare a variable called myName & assign to it a string 
+             //     that represents your Full Name.
+   
+    //   Ans.no-02
+   
+     console.log('my name is ' + userName);
+   
+   
+    //    Q.no-03 : Write script to
+      //      a) Declare a JS variable, titled message.
+       //      b) Assign “Hello World” to variable message
+      //      c) Display the message in alert box
+   
+       //   Ans.no-03
+   
+     //     a) Declare a JS variable, titled message.
+   
+   var message = 'Hello world';
+   
+   //    b) Assign “Hello World” to variable message
+   
+       console.log(message);
+   
+       // c) Display the message in alert box
+   
+       alert(message);
+   
+   
+       // Q-.no-04 : Write a script to save student’s bio data in JS variables and  
+       //         show the data in alert boxes.
+   
+   
+       // Ans.no-04
+   
+       var userName = 'Muhammad Faizan';
+       var age = '19';
+       var bio = 'web and mobile app development';
+   
+       alert(userName);
+       alert(age);
+       alert(bio);
+   
+       // Q.no-05 : Write a script to display the following alert using one JS 
+                 // variable:
+       
+       // Ans.no-05
+   
+       // Define the alert message
+   var alertMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+   
+   // Display the alert
+   alert(alertMessage);
+   
+
+     //    Q.no-06 : Declare a variable called email and assign to it a string that 
+                  // represents your Email Address(e.g. example@example.com). 
+                  // Show the blow mentioned message in an alert box.(Hint: use 
+                  // string concatenation)
+
+            //   Ans.no-06
+
+            // email variable and initialize
+
+            var email = "fiazansuri855@gmail.com";
+
+            // create the message to display in the alert box 
+
+            var message = "My email adress is " + email ;
+
+            // display in this alert box 
+
+            alert(message);
+
+
+            // Q.no-07 : Declare a variable called book & give it the value “A
+                      // smarter way to learn JavaScript”. Display the following
+                      // message in an alert box
+
+
+               // Ans.no-07
+
+            //    declare a variable called book
+
+            var book = "A smarter way to learn javascript";
+
+            alert(book)
+
+
+   
+   
+   
+   
