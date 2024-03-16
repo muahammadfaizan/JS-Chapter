e77746c714ead09f515167754c4af4ebed674ff9
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-17 01:34:51 +0500

    JS variable of number

---
+++ chapter-03/app.js
@@ -0,0 +1,78 @@
+// console.log('variable of number');
+
+    //    chapter 03
+
+    //   Question & Answer
+
+
+    //    Q.No-01 : Declare a variable called age & assign to it your age. Show
+    //              your age in an alert box.    
+
+
+    //   Ans.no-01 :
+    
+    var age = 19 ;
+
+    alert('I am ' + age + ' years old');
+
+
+    // Q.No-02 :  Declare & initialize a variable to keep track of how many
+    //            times a visitor has visited a web page. Show his/her
+    //            number of visits on your web page. For example: “You
+    //            have visited this site N times”.
+    
+
+    // Ans.No-02 :
+
+
+    let visitCount = 14 ;
+    alert('You have visited this site' + visitCount + ' times.')
+
+
+
+    // Q.No-03 :  Declare a variable called birthYear & assign to it your
+    //            birth year. Show the following message in your browser:
+
+     
+    // Ans.No-03 :
+
+    var birthYear = 2005 ;
+
+    document.write('My birth year is ' + birthYear);
+
+
+    // Q.No-04 :  A visitor visits an online clothing store
+    //            www.xyzClothing.com . Write a script to store in variables
+    //            the following information:
+    //         a. Visitor’s name
+    //         b. Product title
+    //         c. Quantity i.e. how many products a visitor wants to
+    //            order
+    //            Show the following message in your browser: “John
+    //             Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+    //    Ans.N0-04 : 
+
+    // var visitorName = "muhammad";
+    // var products = "T-shirt" ;
+    // var quantity = "5";
+
+
+    // document.write(visitorName + " ordered " + quantity + ' ' + products + ' (s)on XYZ clothing store' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
